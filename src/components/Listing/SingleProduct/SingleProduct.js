dd6ca903e444ce3d7d359dad81fec497a3737e34
Oğuz Ölke <oguzolke@gmail.com>
2021-10-25 16:59:47 +0300

    product listing

--- src/components/Listing/SingleProduct/SingleProduct.js
+++ src/components/Listing/SingleProduct/SingleProduct.js
@@ -1,12 +1,36 @@
-import "./SingleProduct.scss";
+import Button from '../../UI/Button/Button';
+import './SingleProduct.scss';
 
-
-const SingleProduct = () => {
-    return (
-        <div className="single-product">
-         Single
+const SingleProduct = ({ product }) => {
+  console.log(product);
+  return (
+    <div className='single-product'>
+      <div className='image-container'>
+        <img src={product.imageURL} alt={product.name} />
+      </div>
+      <p className='product-name'>{product.name}</p>
+      <div className='product-body'>
+        <p className='brand'>
+          <span>Marka: </span>
+          {product.brand}
+        </p>
+        <p className='color'>
+          <span>Renk: </span>
+          {product.color}
+        </p>
+        <p className='new-price'>{product.price} TL</p>
+        <div className='old-price'>
+          <span>
+            {product.price + product.price / (product.discount || 20)} TL
+          </span>
+          <span>{product.discount}%</span>
         </div>
-    )
-}
+      </div>
+      <div className='product-cart-btn'>
+        <Button text='Sepete ekle' />
+      </div>
+    </div>
+  );
+};
 
-export default SingleProduct
+export default SingleProduct;
